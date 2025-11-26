72d8740dfbfd2d4adb39299a5827d12aa9ec0183
tacuma.bellford <tacuma.bellford@lego.com>
2025-11-26 16:18:46 +0000

    Initial commit

---
+++ test/localstack-spike.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LocalstackSpike from '../lib/localstack-spike-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/localstack-spike-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LocalstackSpike.LocalstackSpikeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
